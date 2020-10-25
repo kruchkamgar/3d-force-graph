48f7bae2c2669ebbbbafcc46e49e6cf8514db586
kruchkamgar <kruchkamgar@gmail.com>
2020-10-25 14:26:15 -0600

    correct syntax errors (const)
    up to graph mapping... 'graphLinks'

--- src/lib/prep-graph-dataset.js
+++ src/lib/prep-graph-dataset.js
@@ -1,13 +1,16 @@
-export hierarchicToGraph = function (json){
+const hierarchicToGraph = function (json){
   const nodeList = {};
   const nodeLinks = [];
+  // buildNodeArrays(json, nodeList, nodeLinks);
   json.forEach( node => {
     nodeList[node.id] = node;
-    if (node.children.length > 0) {
-      [nodeLinks, nodeList] = recurseChildren(json, nodeLinks, nodeList)
+    if (node.children && node.children.length > 0) {
+      recurseChildren(node, nodeLinks, nodeList);
     }
-  })
+  });
+
   const graphLinks = nodeLinks.map( link => {
+    debugger;
     return link.map( relations => {
       relations.children.map( child => {
         return {source: relations.parent, target: child};
@@ -22,15 +25,15 @@ export hierarchicToGraph = function (json){
   }
 }
 
-function recurseChildren(json, nodeLinks, nodeList) {
+function buildNodeArrays (json, nodeList, nodeLinks){}
+function recurseChildren(node, nodeLinks, nodeList) {
   // json.forEach(node => {
     nodeList[node.id] = node;
     nodeLinks.push(
       {parent: node.id, children: node.children} );
-    if (node.children.length > 0){
+    if (node.children && node.children.length > 0){
       return recurseChildren(node.children, nodeLinks, nodeList)
     } else {
       return [nodeLinks, nodeList];
     }
-  })
-}
+  }
